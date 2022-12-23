f3fb2aac91d0ffe2cc513d937da82c6b60217c30
brijeshvp <brijeshpeshvani38@gmail.com>
2022-12-24 00:30:12 +0530

    Designed login register and add to cart page

--- server.js
+++ server.js
@@ -12,9 +12,13 @@ let PORT = process.env.PORT || 3000;
 // if not specify this, css and js will not run
 app.use(express.static('public'));
 
-// not working
-// // set template engine
-// app.use(expressLayout); // tells express which layouts to use
+
+// set template engine
+app.use(expressLayout); // tells express which layouts to use
+// this will search layout.ejs in folder we specify as views(so make sure to create layout.ejs in views folder else error)
+// this express-ejs-layouts will help us to avoid writing repetitive code(template inheritance)
+// nav, footer and base ejs code will be kept in layout.ejs
+
 
 // just like any template engine, ejs helps embed data from server side into html dynamically
 app.set('view engine','ejs'); // specify template engine to use
@@ -24,6 +28,15 @@ app.set('views',path.join(__dirname,'/resources/views'));   // specify views/tem
 // endpoints
 app.get('/',(req,res)=>{
     res.render("home");
+})
+app.get('/cart',(req,res)=>{
+    res.render("customers/cart");
+})
+app.get('/login',(req,res)=>{
+    res.render("auth/login");
+})
+app.get('/register',(req,res)=>{
+    res.render("auth/register");
 })
 
 // Start server
